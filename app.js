d8b76030bda33d24632be473bcf76881e2b98753
ufarwa <110379925+ufarwa@users.noreply.github.com>
2023-05-14 09:24:59 +0500

    Add files via upload
    js chptr 3 assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+// //Q1)Declare 3 variables in one statement.
+//let Name,Class,DOB;
+//Name="Farwa";
+//Class="sylani";
+//DOB= 1999;
+
+// //Q2)Declare 5 legal & 5 illegal variable names.
+// legal variable names
+//nameOfStudent= "farwa ";
+//classOfstudent= "sylani ";
+//totalFee$= 5000;
+//class_Name="JS ";
+//studentName_="FARWA ";
+document.write(nameOfStudent  + "<br>"+  classOfstudent +"<br>" +totalFee$ +"<br>" + class_Name +"<br>" + studentName_)
+// illegal variable names
+//names#Student= "farwa ";
+//class(student= "sylani ";
+//totalFee!= 5000;
+//class Name="JS ";
+//studentName%="FARWA ";
